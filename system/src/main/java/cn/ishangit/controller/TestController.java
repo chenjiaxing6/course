ebde332c8bbf7325c19c73de00a730d0651f0975
chen <2609076192@qq.com>
2021-06-17 14:28:08 +0800

    增加system模块，并将system注册到eureka

---
+++ system/src/main/java/cn/ishangit/controller/TestController.java
@@ -0,0 +1,19 @@
+package cn.ishangit.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Chen
+ * @date 2021-06-17
+ * @description
+ */
+@RestController
+public class TestController {
+
+
+    @RequestMapping("/test")
+    public String test(){
+        return "success";
+    }
+}
