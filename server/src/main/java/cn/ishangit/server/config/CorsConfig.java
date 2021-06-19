2909a509b268c8ed6f4088c4ecb3c882c98b68c4
chen <2609076192@qq.com>
2021-06-19 22:13:47 +0800

    集成axios，解决跨域问题

---
+++ server/src/main/java/cn/ishangit/server/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package cn.ishangit.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Chen
+ * @date 2021-06-19
+ * @description 解决跨域问题
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
