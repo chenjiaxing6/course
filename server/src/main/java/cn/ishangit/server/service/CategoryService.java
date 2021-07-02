b32d2aaa8715274bf269b0b855c1aee9bb18a99c
chen <2609076192@qq.com>
2021-07-02 18:33:47 +0800

    1.对选中的表格触发点击事件，以刷新二级分类； 2.删除分类时删除所有二级分类

--- server/src/main/java/cn/ishangit/server/service/CategoryService.java
+++ server/src/main/java/cn/ishangit/server/service/CategoryService.java
@@ -10,6 +10,7 @@ import cn.ishangit.server.util.UuidUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
@@ -79,8 +80,23 @@ public class CategoryService {
      * 删除
      * @param id
      */
+    @Transactional
     public void delete(String id){
+        deleteChild(id);
         categoryMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 删除子分类
+     */
+    public void deleteChild(String id){
+        Category category = categoryMapper.selectByPrimaryKey(id);
+        if ("00000000".equals(category.getParent())){
+            //如果是一级分类  就删除下面的二级分类
+            CategoryExample example = new CategoryExample();
+            example.createCriteria().andParentEqualTo(category.getId());
+            categoryMapper.deleteByExample(example);
+        }
     }
 
 }
